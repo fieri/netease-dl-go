f0d57db82e7e84455aefbb454be42b06a3caafff
KotiyaSanae <dream@trytwice.me>
2019-02-02 20:13:51 +0800

    update main.go

--- main.go
+++ main.go
@@ -11,6 +11,10 @@ import (
 	"github.com/urfave/cli"
 )
 
+var (
+	ids = []string{}
+)
+
 func main() {
 	app := cli.NewApp()
 	app.Name = "netease-dl-go"
@@ -21,56 +25,70 @@ func main() {
 			Name:  "down, d",
 			Usage: "Download song by url. s for song, l for playlist.",
 		},
-		cli.BoolFlag{
-			Name:  "version, V",
-			Usage: "print only the version",
+		// cli.BoolFlag{
+		// 	Name:  "version, V",
+		// 	Usage: "print only the version",
+		// },
+	}
+	app.Commands = []cli.Command{
+		cli.Command{
+			Name:    "down",
+			Aliases: []string{"d"},
+			Usage:   "download song or playlist",
+			Flags: []cli.Flag{
+				cli.BoolFlag{Name: "forever, forevvarr"},
+				cli.BoolFlag{Name: "song, s"},
+				cli.BoolFlag{Name: "list, l"},
+			},
+			Before: func(c *cli.Context) error {
+				for i := 0; i < c.NArg(); i++ {
+					ids = append(ids, basic.GetID(c.Args().Get(i)))
+				}
+				return nil
+			},
+			Action: func(c *cli.Context) error {
+				if c.NArg() <= 0 {
+					return errors.New("arg error: no url or id")
+				}
+				if c.Bool("forever") {
+					// fmt.Println(c.Command.FullName())
+					fmt.Println(c.Command.Names())
+				}
+				if c.Bool("song") {
+					for _, v := range ids {
+						err := netease.DownloadSongByID(v)
+						if err != nil {
+							return err
+						}
+					}
+				}
+				if c.Bool("list") {
+					for _, v := range ids {
+						err := netease.DownloadSongByPlaylist(v)
+						if err != nil {
+							return err
+						}
+					}
+				}
+				return nil
+			},
 		},
 	}
 	cli.AppHelpTemplate = fmt.Sprintf(`%s
 
 EXAMPLE:
-    netease-dl-go --down s 554242032     ;;Download the song which id = 554242032
-    netease-dl-go --down s https://music.163.com/#/song?id=554242032    ;;Download the song which id = 554242032
-    netease-dl-go --down s 554242032 531295576     ;;Download songs id = 554242032 & 531295576 etc.
-    netease-dl-go -d s https://music.163.com/#/song?id=554242032 https://music.163.com/#/song?id=531295576    ;;Download songs id = 554242032 & 531295576
-    netease-dl-go --down l  115707002    ;;Download the playlist which id = 115707002
-    netease-dl-go -d l 115707002 6683129    ;;Download playlists id = 115707002 & 6683129
-    netease-dl-go --down l https://music.163.com/#/my/m/music/playlist?id=115707002    ;;Download the playlist which id = 115707002
-    netease-dl-go -d l https://music.163.com/#/my/m/music/playlist?id=115707002 https://music.163.com/#/my/m/music/playlist?id=6683129    ;;Download playlists id = 115707002 & 6683129
+    netease-dl-go down --song 554242032     ;;Download the song which id = 554242032
+    netease-dl-go down -s https://music.163.com/#/song?id=554242032    ;;Download the song which id = 554242032
+    netease-dl-go d --song 554242032 531295576     ;;Download songs id = 554242032 & 531295576 etc.
+    netease-dl-go d -s https://music.163.com/#/song?id=554242032 https://music.163.com/#/song?id=531295576    ;;Download songs id = 554242032 & 531295576
+    netease-dl-go down --list  115707002    ;;Download the playlist which id = 115707002
+    netease-dl-go down -l 115707002 6683129    ;;Download playlists id = 115707002 & 6683129
+    netease-dl-go d --list https://music.163.com/#/my/m/music/playlist?id=115707002    ;;Download the playlist which id = 115707002
+    netease-dl-go d -l https://music.163.com/#/my/m/music/playlist?id=115707002 https://music.163.com/#/my/m/music/playlist?id=6683129    ;;Download playlists id = 115707002 & 6683129
 
 EMAIL: dream@trytwice.me
 
 `, cli.AppHelpTemplate)
-
-	app.Action = func(c *cli.Context) error {
-		if downVar := c.String("down"); downVar != "s" && downVar != "l" {
-			return errors.New("flag error: do you use the command correctly")
-		}
-		if c.NArg() <= 0 {
-			return errors.New("arg error: no url or id")
-		}
-		ids := []string{}
-		for i := 0; i < c.NArg(); i++ {
-			ids = append(ids, basic.GetID(c.Args().Get(i)))
-		}
-		if c.String("down") == "s" {
-			for _, v := range ids {
-				err := netease.DownloadSongByID(v)
-				if err != nil {
-					return err
-				}
-			}
-		}
-		if c.String("down") == "l" {
-			for _, v := range ids {
-				err := netease.DownloadSongByPlaylist(v)
-				if err != nil {
-					return err
-				}
-			}
-		}
-		return nil
-	}
 
 	err := app.Run(os.Args)
 	if err != nil {
