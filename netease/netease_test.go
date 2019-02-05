882ac4ba2fcb3aca37f62157663161069f684733
KotiyaSanae <dream@trytwice.me>
2019-02-05 21:33:44 +0800

    add benchmark test

--- netease/netease_test.go
+++ netease/netease_test.go
@@ -64,3 +64,9 @@ func TestSearchSong(t *testing.T) {
 		fmt.Printf("%+v\n", v)
 	}
 }
+
+func BenchmarkDownloadSongByPlaylistCur(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = DownloadSongByPlaylistCur(pid)
+	}
+}
